6a3dcf4326f0c7ffc7aff4dc74bdaf65f816cb3a
ira <irena.vidrak@autodesk.com>
2019-12-22 13:18:34 +0200

    prepare environment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("bla")
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PUBLIC = path.join(__dirname, 'public');
+app.use(express.static(PUBLIC));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Expose-Headers', '*');
+    next();
+});
+
+
+console.log('app started..');
+/**
+ * Swagger initialization on top of express
+ */
+
+console.log('[lifecycle]: core service is booting up');
+
+const port = 5000;
+app.listen(port);
+console.log('[lifecycle]: core service is now listening', {
+    port,
+});
+
+module.exports = {
+    server: app,
+};
