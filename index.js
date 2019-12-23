705dbd6b0f70d3c2105b179f0748d223af42b3dc
itzikyacobi <itzhak.yacobi@autodesk.com>
2019-12-23 11:34:48 +0200

    adding socket

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const http = require('http').createServer(app);
-const socketIO = require('socket.io')(http);
+const http = require('http');
+const socketIO = require('socket.io');
 
 const PUBLIC = path.join(__dirname, 'public');
 app.use(express.static(PUBLIC));
@@ -22,15 +22,38 @@ console.log('app started..');
 console.log('[lifecycle]: core service is booting up');
 
 const port = process.env.PORT || 5000;
-app.listen(port);
 console.log('[lifecycle]: core service is now listening', {
     port,
 });
 
-socketIO.on('connection', function(socket){
-    console.log('a user connected');
+app.get('/health', function(request, response) {
+    console.log('got request for path /health ');
+    response.send({health:'ok',
+        serverCurrentTimeAsString: `${new Date()}`,
+        serverCurrentTime: (new Date()),
+        nodeVersion: process.version}
+    );
 });
 
+const server = http.createServer(app);
+const io = socketIO.listen(server);
+
+server.listen(5000, function() {
+    console.log('### startListening ##');
+    console.log(`Node app is running on port:  5000`);
+    io.on('connection', function (socket) {
+        console.log(`a user connected`);
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+        socket.on('chat message', function(msg){
+            console.log('message: ' + msg);
+            socket.emit('chat response', "cool i'm server");
+        });
+    });
+});
+
+
 module.exports = {
-    server: app,
+    server
 };
